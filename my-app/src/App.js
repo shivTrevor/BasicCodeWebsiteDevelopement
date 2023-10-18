000f6ba3bfc1761847881adc719dc08aa303cb09
Shivnandan Tiwari <36485361+shivTrevor@users.noreply.github.com>
2023-10-18 13:41:48 +0530

    Recipe manager

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header >
+          Recipe manager application
+      </header>
+    </div>
+  );
+}
+
+export default App;
