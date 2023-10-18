f23e295b52410a18912f890d576008eae447127b
Shivnandan Tiwari <36485361+shivTrevor@users.noreply.github.com>
2023-10-18 19:50:05 +0530

    main screen - app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,25 @@
-
-import './App.css';
+import React, { useState } from 'react';
+import RecipeList from './recipeList'; // Importing RecipeList component
+import RecipeForm from './recipeForm'; // Importing RecipeForm component
+import './App.css'
 
 function App() {
+  // State to manage the list of recipes
+  const [recipes, setRecipes] = useState([]);
+
+  // Function to add a new recipe to the list
+  const handleAddRecipe = (name) => {
+    setRecipes([...recipes, { name }]); // Updating recipes state with a new recipe
+  };
+
   return (
-    <div >
-      <header >
-          Recipe manager application
-      </header>
+    <div>
+      <h1>Recipe Manager</h1> {/* Header */}
+      {/* Rendering RecipeList and RecipeForm components */}
+      <RecipeList recipes={recipes} /> {/* Rendering RecipeList with recipes as prop */}
+      <RecipeForm onAddRecipe={handleAddRecipe} /> {/* Rendering RecipeForm with onAddRecipe function */}
     </div>
   );
 }
 
-export default App;
+export default App; // Exporting the App component as default
