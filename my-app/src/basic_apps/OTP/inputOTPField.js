96b35544008a6fb82d4da8d585d6b10c22ee2e20
Shivnandan Tiwari <36485361+shivTrevor@users.noreply.github.com>
2023-12-30 10:43:13 +0530

    Update inputOTPField.js
    updated with the handle global click function ,

--- my-app/src/basic_apps/OTP/inputOTPField.js
+++ my-app/src/basic_apps/OTP/inputOTPField.js
@@ -1,53 +1,63 @@
-import React, { useState } from 'react';
-import './rough.css'
+import React, { useState, useRef, useEffect } from 'react';
+import './trialCSS.css';
 
 const OtpContainer = () => {
   const [otpValues, setOtpValues] = useState(['', '', '', '']);
+  const inputRefs = useRef(Array(4).fill(null).map(() => React.createRef()));
 
-  const handleInputChange = (index, value ) => {
-    setOtpValues(prevOtpValues => {
+  const handleInputChange = (index, value) => {
+    setOtpValues((prevOtpValues) => {
       const newOtpValues = [...prevOtpValues];
       newOtpValues[index] = value;
-  
-        // this is for the shifting of focus...
-    if (value && index < 3) {
-      const nextInput = document.getElementById(`digit${index+1}`);
-      
-      // Check if the next input element exists before focusing
-      if (nextInput) {
-        nextInput.focus();
 
-        // Prevent the default behavior of clearing the input value
-        nextInput.addEventListener('focus', (e) => e.preventDefault());
+      // this is for the shifting of focus...
+      if (value && index < 3) {
+        inputRefs.current[index + 1].current.focus();
       }
-    }
 
       // show the current value in the console log
       console.log(newOtpValues);
-  
+
       return newOtpValues;
     });
   };
 
   const handleKeyDown = (e, index) => {
     if (e.key === 'Backspace' && index > 0) {
-      //set the current element to 
-      setOtpValues(prevOtpValues => {
+      // set the current element to
+      setOtpValues((prevOtpValues) => {
         const newOtpValues = [...prevOtpValues];
         newOtpValues[index] = '';
-         return newOtpValues;
+        return newOtpValues;
       });
 
-      const currentInput = document.getElementById(`digit${index-1}`);
+      inputRefs.current[index - 1].current.focus();
       
-      // Check if the next input element exists before focusing
-      if (currentInput) {
-        e.preventDefault();
-        currentInput.focus();
-      }
     }
   };
-  
+
+  //so bro here with respond to the global event listener bro i will kept on focusing the current index 
+  const handleGlobalClick = (e) => {
+    const otpContainer = document.querySelector('.otp-container');
+
+    if (otpContainer && !otpContainer.contains(e.target)) {
+      // Clicked outside the OTP container, prevent the default behavior
+      e.preventDefault();
+    }
+
+    //show in the last bro 
+    inputRefs.current[3].current.focus();
+  };
+
+  useEffect(() => {
+    // Add a global click event listener
+    document.addEventListener('click', handleGlobalClick);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      document.removeEventListener('click', handleGlobalClick);
+    };
+  }, []);
 
   return (
     <div className="otp-container">
@@ -60,6 +70,10 @@ const OtpContainer = () => {
           onChange={(e) => handleInputChange(index, e.target.value)}
           onKeyDown={(e) => handleKeyDown(e, index)}
           id={`digit${index}`}
+          autoFocus={index === 0} // Set autoFocus on the first input
+          onMouseDown={(e) => e.preventDefault()} // Prevent focus from leaving on click
+          className="otp-input"
+          ref={inputRefs.current[index]}
         />
       ))}
     </div>
