e0a7c3d732e589272dce0d4ae5f7d0d2aee5ab1e
Shivnandan Tiwari <36485361+shivTrevor@users.noreply.github.com>
2024-01-07 10:20:54 +0530

    Update index.js
    email entering feild

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import EmailEntering from './basic_apps/OTP/plainHTMLCSSJS/emailEntering'
 // import OTP from './basic_apps/OTP/twilioOtpGenerator.js'
-import Rough from './basic_apps/OTP/inputOTPField'
+import InputOTP from './basic_apps/OTP/inputOTPField'
+import Router from './concepts/conceptReact/navigation/Main'
+import Rough from './basic_apps/OTP/trialReact'
+import SingleRef from './basic_apps/OTP/singleRef'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
- <Rough />
+  <EmailEntering />
   </React.StrictMode>
 );
 
