cd6542137be025028bd90be277b56492a09fc4ac
Shivnandan Tiwari <36485361+shivTrevor@users.noreply.github.com>
2024-01-10 19:12:10 +0530

    Create frontEnd.js
    creating an api ...to connect with the backend

---
+++ my-app/src/concepts/conceptReact/dataFlowBackendAndFrontend/frontEnd.js
@@ -0,0 +1,30 @@
+// MyComponent.js
+
+import React, { useEffect, useState } from 'react';
+
+const MyComponent = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/api/data');
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h2>Data from Backend:</h2>
+      {data && <p>{data.message}</p>}
+    </div>
+  );
+};
+
+export default MyComponent;
