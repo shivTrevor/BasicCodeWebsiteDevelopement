c47209ca6408a86c26ff533af246ae6e853d9b8d
Shivnandan Tiwari <36485361+shivTrevor@users.noreply.github.com>
2024-01-15 13:02:40 +0530

    backend of the todolist

---
+++ my-app/src/concepts/conceptReact/CRUDOperationUsingTODOLIST/backEnd.js
@@ -0,0 +1,55 @@
+// server.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors())
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/dataFlow', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const ItemSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+});
+
+const Item = mongoose.model('Item', ItemSchema);
+
+// Create
+app.post('/api/items', async (req, res) => {
+  const newItem = new Item(req.body);
+  await newItem.save();
+  res.json(newItem);
+});
+
+// Read
+app.get('/api/items', async (req, res) => {
+  const items = await Item.find();
+  res.json(items);
+});
+
+// Update
+app.put('/api/items/:id', async (req, res) => {
+  const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.json(updatedItem);
+});
+
+// Delete
+app.delete('/api/items/:id', async (req, res) => {
+  await Item.findByIdAndRemove(req.params.id);
+  res.json({ success: true });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
